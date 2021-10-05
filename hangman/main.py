865be99688a8e0504158bd1e95fa74b7183fc068
kristaapsm <kristaps.murnieks@gmail.com>
2021-10-05 17:12:43 +0300

    pievienots loggeris

--- hangman/main.py
+++ hangman/main.py
@@ -1,66 +1,21 @@
 #!/usr/bin/python3
 # Hangman game
 
-#Imports priekš žurnalēšanas
-
 import logging
-import logging.config
-#imports prieksh db
+# imports prieksh db
 import mysql.connector
-#imports prieksh koda
+# imports prieksh koda
 import random
-# import lai varētu nolasīt žurnalēšanas info
-import yaml
+from datetime import date
 
-from configparser import ConfigParser
-from mysql.connector import Error
-
-
-# Loading logging configuration
-with open('./log_worker.yaml', 'r') as stream:
-    config = yaml.safe_load(stream)
-
-logging.config.dictConfig(config)
-
-# Creating logger
-logger = logging.getLogger('root')
-
-logger.info('Asteroid processing service')
-
-# Initiating and reading config values
-logger.info('Loading configuration from file')
-
-try:
-	config = ConfigParser()
-	config.read('config.ini')
-
-	mysql_config_mysql_host = config.get('mysql_config', 'mysql_host')
-	mysql_config_mysql_db = config.get('mysql_config', 'mysql_db')
-	mysql_config_mysql_user = config.get('mysql_config', 'mysql_user')
-	mysql_config_mysql_pass = config.get('mysql_config', 'mysql_pass')
-
-except:
-	logger.exception('')
-logger.info('DONE')
-
-# default value datubāzei
-connection = None
-connected = False
-
-
-# Inicializē datubāzi
-def init_db():
-	global connection
-	connection = mysql.connector.connect(host=mysql_config_mysql_host, database=mysql_config_mysql_db, user=mysql_config_mysql_user, password=mysql_config_mysql_pass)
-
-init_db()
-
+# žurnalēšanas komandrinda
+logging.basicConfig(filename='hangman_log.log', level=logging.DEBUG, format='%(asctime)s %(message)s')
 
 
 # kārātuves zīmējums
 class HangMan(object):
     # Hangman game
-    #hang variable visu apvieno kopā lai izveidotu karātuves
+    # hang variable visu apvieno kopā lai izveidotu karātuves
     hang = []
     hang.append(' +---+')
     hang.append(' |   |')
@@ -69,7 +24,7 @@ class HangMan(object):
     hang.append('     |')
     hang.append('     |')
     hang.append('=======')
-#man variable izveido cilvēciņu
+    # man variable izveido cilvēciņu
     man = {}
     man[0] = [' 0   |']
     man[1] = [' 0   |', ' |   |']
@@ -79,7 +34,7 @@ class HangMan(object):
     man[5] = [' 0   |', '/|\\  |', '/ \\  |']
 
     pics = []
-# words variable ir vārdu kopums no kura paņem random vārdu un to izmanto priekš spēles
+    # words variable ir vārdu kopums no kura paņem random vārdu un to izmanto priekš spēles
     words = '''ant baboon badger bat bear beaver camel cat clam cobra cougar coyote
 crow deer dog donkey duck eagle ferret fox frog goat goose hawk lion lizard llama
 mole monkey moose mouse mule newt otter owl panda parrot pigeon python rabbit ram
@@ -87,13 +42,6 @@ rat raven rhino salmon seal shark sheep skunk sloth snake spider stork swan tige
 toad trout turkey turtle weasel whale wolf wombat zebra'''.split()
 
     infStr = '_-*\'*-_-*\'*-_-*\'*-_-*\'*-_-*\'*-_-*\'*-_-*\'*-_-*\'*-_-*\'*-_-*\''
-
-    def init_db():
-        global connection
-        connection = mysql.connector.connect(host=mysql_config_mysql_host, database=mysql_config_mysql_db,
-                                             user=mysql_config_mysql_user, password=mysql_config_mysql_pass)
-
-    init_db()
 
     def __init__(self, *args, **kwargs):
         i, j = 2, 0
@@ -104,6 +52,7 @@ toad trout turkey turtle weasel whale wolf wombat zebra'''.split()
                 pic[i + j] = m
                 j += 1
             self.pics.append(pic)
+
     # izvēlas random vārdu
     def pickWord(self):
         return self.words[random.randint(0, len(self.words) - 1)]
@@ -114,6 +63,7 @@ toad trout turkey turtle weasel whale wolf wombat zebra'''.split()
 
     def askAndEvaluate(self, word, result, missed):
         guess = input()
+        logging.info('Player chose letter '+  guess)
         if guess == None or len(guess) != 1 or (guess in result) or (guess in missed):
             return None, False
         i = 0
@@ -131,6 +81,7 @@ toad trout turkey turtle weasel whale wolf wombat zebra'''.split()
         print(self.infStr[3:])
 
     def start(self):
+        logging.info('***NEW GAME***')
         print('Welcome to Hangman !')
         word = list(self.pickWord())
         result = list('*' * len(word))
