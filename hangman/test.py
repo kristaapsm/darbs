d94b758453c57ad3239c9e04f12cca850ab1db12
kristaapsm <kristaps.murnieks@gmail.com>
2021-11-23 19:01:38 +0200

    added script to check for files

--- hangman/test.py
+++ hangman/test.py
@@ -1,5 +1,4 @@
 import os
-import mysql.connector
 from configparser import ConfigParser
 
 
@@ -22,17 +21,6 @@ print("OK")
 print("-----------------")
 
 
-print("Checking connection to database")
-mysql_config_mysql_host = config.get('mysql_config', 'mysql_host')
-mysql_config_mysql_db = config.get('mysql_config', 'mysql_db')
-mysql_config_mysql_user = config.get('mysql_config', 'mysql_user')
-mysql_config_mysql_pass = config.get('mysql_config', 'mysql_pass')
-
-connection = mysql.connector.connect(host=mysql_config_mysql_host, database=mysql_config_mysql_db,
-                                    user=mysql_config_mysql_user, password=mysql_config_mysql_pass)
-assert connection.is_connected() == True
-print("OK")
-print("-----------------")
 
 print("Checking if directory log exists")
 assert os.path.isdir("log")
